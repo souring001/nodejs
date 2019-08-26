f4ffe4f67f35c337efaeebefdee8e420c199249e
aso <titechei@gmail.com>
2019-08-26 17:41:43 +0900

    switch by req.url

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var settings = require('./settings')
 console.log(settings);
 var server =  http.createServer();
 server.on('request', function(req, res) {
+    switch (req.url){
+        case '/about':
+            msg = 'about this page';
+            break;
+        case '/profile':
+            msg = 'about me'
+            break;
+        default:
+            msg = 'wrong page'
+    }
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world');
+    res.write(msg);
     res.end();
 });
 server.listen(settings.port, settings.host);
