a59d88777f9426f0fca1c6a51877d4294b136a0e
aso <titechei@gmail.com>
2019-08-26 17:28:10 +0900

    Add server.js

--- hello.js
+++ hello.js
@@ -5,3 +5,16 @@ setTimeout(function(){
     console.log('hello');
 }, 1000);
 console.log('world');
+
+// how about this? -> compile error
+/*
+setTimeout(console.log('hello');, 1000);
+console.log('world');
+*/
+
+// blocking
+/*
+var start = new Date().getTime();
+while (new Date().getTime() < start + 1000);
+console.log('world');
+*/
