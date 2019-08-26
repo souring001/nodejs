34dcf5feee3bf7b4cfb132eac8ccd3b13c01fba5
aso <titechei@gmail.com>
2019-08-26 17:46:40 +0900

    Read hello.html

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 var settings = require('./settings')
 console.log(settings);
 var server =  http.createServer();
 server.on('request', function(req, res) {
-    switch (req.url){
-        case '/about':
-            msg = 'about this page';
-            break;
-        case '/profile':
-            msg = 'about me'
-            break;
-        default:
-            msg = 'wrong page'
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    res.end();
+    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('not found!');
+            return  res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 server.listen(settings.port, settings.host);
 console.log('server listening ...')
