2dc9dae27c8af8fae7bc8c01495f3f53ea2d9f78
aso <titechei@gmail.com>
2019-09-01 00:04:48 +0900

    Find db

--- mongo.js
+++ mongo.js
@@ -1,8 +1,9 @@
 var MongoClient = require('mongodb').MongoClient,
     settings = require('./settings');
-MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, db){
+MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, client){
     if(err) { return console.dir(err); }
     console.log("connected to db");
+    const db = client.db(settings.db);
     db.collection("users", function(err, collection){
         if(err) { return console.dir(err); }
         var docs = [
@@ -10,9 +11,21 @@ MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, db
             {name: "gotoh", score: 80},
             {name: "ishibashi", score: 60}
         ];
-        collection.insert(docs, function(err, result){
-            if(err) { return console.dir(err); }
-            console.dir(result);
+        // collection.insert(docs, function(err, result){
+        //     if(err) { return console.dir(err); }
+        //     console.dir(result);
+        // });
+
+        // collection.find({name: "gotoh"}).toArray(function(err, items){
+        //     console.log(items);
+        // });
+
+        var stream = collection.find({name: "gotoh"}).stream();
+        stream.on("data", function(item){
+            console.log(item);
+        });
+        stream.on("end", function(){
+            console.log("finished.");
         });
     });
 });
